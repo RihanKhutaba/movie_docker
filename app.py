486d77d4045804c042f56b8d1de6918525fcdb91
RihanKhutaba <100447571+RihanKhutaba@users.noreply.github.com>
2022-05-09 01:41:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import requests
 from pymongo import MongoClient
 
 
-client = MongoClient("localhost", 27017)
+#client = MongoClient("localhost", 27017)
+client = MongoClient("db", 27017)
 
 db = client['poster_name']
 
